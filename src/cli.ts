a9a8c5cf6b064f0fdcdd827c16ea9d21b4dfade1
Anthony <adferra24@gmail.com>
2023-03-04 15:03:38 -0500

    updates

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,65 @@
 #!/usr/bin/env node
-import { main } from './index'
+//import { main } from './index.js';
+import welcome from 'cli-welcome';
+import inquirer from 'inquirer';
+import inquirerFileTreeSelection from 'inquirer-file-tree-selection-prompt';
+import chalk from 'chalk';
+import path from 'path';
+import JSONFormatter from './formatter.js';
+//import * as url from 'node:url';
+//const __filename = url.fileURLToPath(import.meta.url);
+//const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-main()
+(async function cli() {
+    inquirer.registerPrompt('file-tree-selection', inquirerFileTreeSelection);
+    welcome({
+        title: 'JSON Formatter',
+        tagLine: 'by me',
+        description: `A simple CLI to format a folder of json files into a one output file. Default formatting removes the outer array and concatenate all the json files into a single array.`,
+        version: '0.0.1',
+        bgColor: '#0066FF',
+        color: '#000000',
+        bold: true,
+        clear: true,
+    });
+
+    inquirer
+        .prompt([
+            {
+                type: 'file-tree-selection',
+                name: 'inputPath',
+                message: 'select input folder',
+                enableGoUpperDirectory: true,
+                transformer(input) {
+                    const name = input.split(path.sep).pop();
+                    if (name[0] === '.') {
+                        return chalk.grey(name);
+                    }
+                    return name;
+                },
+                onlyShowDir: false,
+            },
+            {
+                type: 'file-tree-selection',
+                name: 'outputPath',
+                message: 'select output folder',
+                transformer(input) {
+                    const name = input.split(path.sep).pop();
+                    if (name[0] === '.') {
+                        return chalk.grey(name);
+                    }
+                    return name;
+                },
+                onlyShowDir: false,
+            },
+            {
+                type: 'input',
+                name: 'outputFileName',
+                message: 'name of formatted output file: ',
+            },
+        ])
+        .then((answers: any) => {
+            const formatter = new JSONFormatter(answers);
+            formatter.init();
+        });
+})();
--- src/formatter.ts
+++ src/formatter.ts
@@ -1,31 +1,50 @@
-const fs = require('fs').promises
+import fs from 'fs';
 //const readline = require('readline')
 
-export class Formatter {
-  constructor(private index: number) {
-    console.log('Handler created', this.index)
-  }
+export default class JSONFormatter {
+    opts: any;
+    resolved: any;
 
-  json: any = []
-  readJSON(err: any, data: any) {
-    if (err) {
-      throw new Error(err)
-    } else {
-      return JSON.parse(data)
+    constructor(private inputs: any) {
+        this.opts = this.inputs;
+        console.log('JSON Formatter instantiated');
     }
-  }
 
-  writeToFile(data: any) {
-    fs.writeFile(`src/json/formatted.json`, JSON.stringify(data), 'utf8', (err: any) => {
-      if (err) {
-        throw new Error(err)
-      } else {
-        console.log('File written')
-      }
-    })
-  }
+    json: any = [];
+    readJSON(err: any, data: any) {
+        if (err) {
+            throw new Error(err);
+        } else {
+            return JSON.parse(data);
+        }
+    }
 
-  formatJSON(data: any) {
-    return data.reduce((prev: any, data: any) => [...prev, ...JSON.parse(data)], [])
-  }
+    writeToFile(data: any) {
+        const { outputPath, outputFileName } = this.opts;
+        fs.writeFile(`${outputPath}/${outputFileName}`, JSON.stringify(data), 'utf8', (err: any) => {
+            if (err) {
+                throw new Error(err);
+            } else {
+                console.log(`File written to ${outputPath}/${outputFileName}`);
+            }
+        });
+    }
+
+    private formatJSON(data: any) {
+        return data.reduce((prev: any, data: any) => [...prev, ...JSON.parse(data)], []);
+    }
+
+    private async readFolder() {
+        const { inputPath } = this.opts;
+        const folder = fs.readdirSync(inputPath);
+        const filePromises = folder.map((file: any) => {
+            return fs.promises.readFile(`${inputPath}/${file}`, 'utf8');
+        });
+        return await Promise.all(filePromises);
+    }
+
+    async init() {
+        this.resolved = await this.readFolder();
+        this.writeToFile(this.formatJSON(this.resolved));
+    }
 }
